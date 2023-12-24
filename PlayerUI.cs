adc991bb482618b865a0888f87924bcf06e86b9a
olaponomareva <150163800+olaponomareva@users.noreply.github.com>
2023-12-24 19:42:30 +0300

    Add files via upload

---
+++ PlayerUI.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerUI : MonoBehaviour
+{
+	public Player player;
+	public TextMeshProUGUI coinsCounterText;
+	public Slider healthSlider;
+
+	void Update()
+	{
+		// Обновляем значение здоровья игрока
+		healthSlider.maxValue = player.maxHealth;
+		healthSlider.value = player.health;
+
+		// Обновляем текст с кол-вом монеток
+		coinsCounterText.text = player.coins.ToString();
+	}
+}
